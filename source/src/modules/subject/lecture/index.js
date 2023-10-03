cc1b93cfc2bd39ddf1331a83d6dfab91b4cc4546
HuynhKhoaIT <>
2023-10-03 23:07:12 +0700

    CRUD Lecture

---
+++ source/src/modules/subject/lecture/index.js
@@ -0,0 +1,103 @@
+import ListPage from '@components/common/layout/ListPage';
+import React from 'react';
+import PageWrapper from '@components/common/layout/PageWrapper';
+import {  DEFAULT_TABLE_ITEM_SIZE } from '@constants';
+import apiConfig from '@constants/apiConfig';
+import useListBase from '@hooks/useListBase';
+import useTranslate from '@hooks/useTranslate';
+import { defineMessages } from 'react-intl';
+import BaseTable from '@components/common/table/BaseTable';
+import { useParams } from 'react-router-dom';
+
+const message = defineMessages({
+    objectName: 'Bài giảng',
+    home: 'Trang chủ',
+    student: 'Học viên',
+    subject: 'Môn học',
+    description: 'Mô tả chi tiết',
+    lectureKind: 'Lecture Kind',
+    shortDescription: 'Mô tả Ngắn',
+    lectureName: 'Tên bài giảng',
+    ordering: 'Ordering',
+    status: 'Trạng thái',
+});
+
+const LectureListPage = () => {
+
+    const translate = useTranslate();
+    const paramid = useParams();
+
+    const { data, mixinFuncs, queryFilter, loading, pagination, changePagination } = useListBase({
+        apiConfig: {
+            getList: apiConfig.lecture.getBySubject,
+            delete: apiConfig.lecture.delete,
+            update: apiConfig.lecture.update,
+            getById:apiConfig.lecture.getById,
+        },
+        options: {
+            pageSize: DEFAULT_TABLE_ITEM_SIZE,
+            objectName: translate.formatMessage(message.objectName),
+        },
+        override: (funcs) => {
+            funcs.prepareGetListPathParams = () => {
+                return {
+                    subjectId : paramid.subjectId,
+                };
+            };
+            funcs.mappingData = (response) => {
+                console.log(response);
+                try {
+                    if (response.result === true) {
+                        return {
+                            data: response.data.content,
+                            total: response.data.totalElements,
+                        };
+                    }
+                } catch (error) {
+                    return [];
+                }
+            };
+        },
+    });
+
+    const columns = [
+        {
+            title: translate.formatMessage(message.lectureName),
+            dataIndex: 'lectureName',
+        },
+        {
+            title: translate.formatMessage(message.description),
+            dataIndex: 'description',
+        },
+        {
+            title: translate.formatMessage(message.shortDescription),
+            dataIndex: 'shortDescription',
+        },
+        mixinFuncs.renderActionColumn({ edit: true, delete: true }, { width: '120px' }),
+    ];
+    return (
+        <PageWrapper 
+            routes={[
+                { breadcrumbName: translate.formatMessage(message.home) },
+                { breadcrumbName: translate.formatMessage(message.subject),
+                    path: `/subject` },
+                { breadcrumbName: translate.formatMessage(message.objectName) },
+            ]}
+        >
+            <ListPage
+                actionBar={mixinFuncs.renderActionBar()}
+                baseTable={
+                    <BaseTable
+                        onChange={changePagination}
+                        pagination={pagination}
+                        loading={loading}
+                        dataSource={data}
+                        columns={columns}
+                    />
+                }
+            />
+        </PageWrapper>
+    );
+};
+
+export default LectureListPage;
---
+++ source/src/modules/subject/lecture/lectureSavePage.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import apiConfig from '@constants/apiConfig';
+import routes from '../routes';
+import PageWrapper from '@components/common/layout/PageWrapper';
+import LectureForm from './lectureForm';
+import useTranslate from '@hooks/useTranslate';
+import useSaveBase from '@hooks/useSaveBase';
+import { generatePath, useParams } from 'react-router-dom';
+import { defineMessages } from 'react-intl';
+const message = defineMessages({
+    objectName:'Bài giảng',
+    subject: 'Môn học',
+    lecture: 'Bài giảng',
+    home:'Trang chủ',
+});
+
+const LectureSavePage = () => {
+    const translate = useTranslate();
+    const lectureId = useParams();
+    const { detail, onSave, mixinFuncs,setIsChangedFormValues,isEditing,errors, loading, title } = useSaveBase({
+        apiConfig: {
+            getById: apiConfig.lecture.getById,
+            create: apiConfig.lecture.create,
+            update: apiConfig.lecture.update,
+        },
+        options:{
+            getListUrl: generatePath(routes.lectureListPage.path, { subjectId : lectureId.subjectId }),
+            objectName: translate.formatMessage(message.objectName),
+        },
+        override: (funcs) => {
+            funcs.prepareUpdateData = (data) => {
+                return {
+                    ...data,
+                    id: detail.id,
+                };
+            };
+            funcs.prepareCreateData = (data) => {
+                return {
+                    ...data,
+                };
+            };
+        },
+
+    });
+    return(
+        <PageWrapper
+            loading = {loading}
+            routes={[
+                { breadcrumbName: translate.formatMessage(message.home) },
+                { breadcrumbName: translate.formatMessage(message.lecture) },
+                { breadcrumbName: translate.formatMessage(message.subject),
+                    path: routes.subjectListPage.path +`/lecture/get-by-subject/${lectureId.subjectId}`,
+                },
+                { breadcrumbName: title },
+            ]}
+            title={title}
+        >
+            <LectureForm
+                subjectId = {lectureId.subjectId}
+                formId={mixinFuncs.getFormId()}
+                actions = {mixinFuncs.renderActions()}
+                dataDetail = {detail ? detail : {}}
+                onSubmit = {onSave}
+                setIsChangedFormValues = {setIsChangedFormValues}
+                isError = {errors}
+                isEditing = {isEditing}
+            />
+        </PageWrapper>
+    );
+};
+export default LectureSavePage;
