2882260df95646adfbc48ea686c0783599aadb0c
Vttien01 <tientienpham01@gmail.com>
2023-10-04 10:53:03 +0700

    fix autocomplete, category(university,studyClass)

--- source/src/modules/student/studentForm.js
+++ source/src/modules/student/studentForm.js
@@ -26,7 +26,7 @@ const message = defineMessages({
     // university: 'university',
 });
 
-const StudentForm = ({ isEditing, formId, actions, dataDetail, onSubmit, setIsChangedFormValues }) => {
+const StudentForm = ({ isEditing, formId, actions, dataDetail, onSubmit, setIsChangedFormValues, handleFocus }) => {
     const translate = useTranslate();
     const kindOfEdu = categoryKinds.CATEGORY_KIND_EDUCATION;
     const kindOfGen = categoryKinds.CATEGORY_KIND_GENERATION;
@@ -125,6 +125,7 @@ const StudentForm = ({ isEditing, formId, actions, dataDetail, onSubmit, setIsCh
                             }}
                             searchParams={(text) => ({ name: text })}
                             disabled={isEditing}
+                            onFocus={handleFocus}
                             required
                         />
                     </Col>
