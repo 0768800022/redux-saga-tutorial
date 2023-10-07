64b306359138abf50ddcb9b99238ab9cfe5df862
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-10-07 17:05:00 +0700

    fix commit conflict

--- source/src/modules/registration/index.js
+++ source/src/modules/registration/index.js
@@ -27,6 +27,7 @@ const message = defineMessages({
     isIntern: 'Đăng kí thực tập',
     course: 'Khóa học',
     registration: 'Danh sách sinh viên đăng kí khóa học',
+    state: 'Trạng thái',
 });
 
 function RegistrationListPage() {
@@ -149,8 +150,18 @@ function RegistrationListPage() {
             ]}
         >
             <ListPage
-                title={<p style={{ fontSize: '18px' }}>Khóa học: <span style={{ fontWeight: 'normal' }}>{courseName}</span></p>}
-                actionBar={mixinFuncs.renderActionBar()}
+                title={
+                    <span
+                        style={
+                            courseState != 5
+                                ? { fontWeight: 'normal', fontSize: '16px' }
+                                : { fontWeight: 'normal', fontSize: '16px', position: 'absolute' }
+                        }
+                    >
+                        {courseName}
+                    </span>
+                }
+                actionBar={courseState == 5 && mixinFuncs.renderActionBar()}
                 baseTable={
                     <BaseTable
                         onChange={changePagination}
