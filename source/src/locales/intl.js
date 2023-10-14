6d9835bd75b20ea226f1c77c9f4d65fbadf77920
Nguyen Van Son <son17112001@gmail.com>
2023-10-14 23:06:44 +0700

    config intl file

---
+++ source/src/locales/intl.js
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+export const commonMessage = defineMessages({
+    home: 'Trang chủ',
+    companyName: 'Tên công ty',
+    address: 'Địa chỉ',
+    createdDate: 'Ngày tạo',
+    modifiedDate: 'Ngày sửa đổi',
+    email: 'Email',
+    holine: 'Holine',
+    logo: 'Logo',
+    status: 'Trạng thái',
+    username: 'Tài khoản đăng nhập',
+    company: 'Công ty',
+    registration: 'Dịch vụ',
+});
