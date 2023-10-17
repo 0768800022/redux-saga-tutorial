d05e527d5ee41ae273e9022e67add717ba8be12e
Edward3103 <86288625+Edward3103@users.noreply.github.com>
2023-10-17 18:24:05 +0700

    change module struct project

---
+++ source/src/modules/projectManage/project/projectStudent/index.js
@@ -0,0 +1,201 @@
+import ListPage from '@components/common/layout/ListPage';
+import React, { useEffect, useState } from 'react';
+import PageWrapper from '@components/common/layout/PageWrapper';
+import { DEFAULT_FORMAT, DATE_FORMAT_DISPLAY, DEFAULT_TABLE_ITEM_SIZE, AppConstants } from '@constants';
+import apiConfig from '@constants/apiConfig';
+import useListBase from '@hooks/useListBase';
+import useTranslate from '@hooks/useTranslate';
+import { defineMessages } from 'react-intl';
+import BaseTable from '@components/common/table/BaseTable';
+import dayjs from 'dayjs';
+import { UserOutlined } from '@ant-design/icons';
+import { Button, Avatar, Tag } from 'antd';
+import { generatePath, useLocation, useNavigate } from 'react-router-dom';
+import { convertDateTimeToString, convertStringToDateTime } from '@utils/dayHelper';
+import routes from '@routes';
+import route from '@modules/projectManage/project/projectTask/routes';
+import { BookOutlined, TeamOutlined, EditOutlined } from '@ant-design/icons';
+import { statusOptions, projectTaskState } from '@constants/masterData';
+import { FieldTypes } from '@constants/formConfig';
+import AvatarField from '@components/common/form/AvatarField';
+import { IconBrandTeams } from '@tabler/icons-react';
+import { BaseTooltip } from '@components/common/form/BaseTooltip';
+import { commonMessage } from '@locales/intl';
+const message = defineMessages({
+    objectName: 'dự án',
+    code: 'Mã dự án',
+    id: 'Id',
+    group: 'Nhóm',
+    startDate: ' Ngày bắt đầu',
+});
+
+const ProjectStudentListPage = () => {
+    const translate = useTranslate();
+    const navigate = useNavigate();
+    const queryParameters = new URLSearchParams(window.location.search);
+    const stateValues = translate.formatKeys(projectTaskState, ['label']);
+    const leaderName = queryParameters.get('leaderName');
+    const developerName = queryParameters.get('developerName');
+    let { data, mixinFuncs, queryFilter, loading, pagination, changePagination, queryParams, serializeParams } =
+        useListBase({
+            apiConfig: {
+                getList: apiConfig.project.getListStudent,
+                getById: apiConfig.project.getById,
+                delete: apiConfig.project.delete,
+            },
+            options: {
+                pageSize: DEFAULT_TABLE_ITEM_SIZE,
+                objectName: translate.formatMessage(message.objectName),
+            },
+            override: (funcs) => {
+                funcs.mappingData = (response) => {
+                    if (response.result === true) {
+                        return {
+                            data: response.data.content,
+                            total: response.data.totalElements,
+                        };
+                    }
+                };
+
+                funcs.changeFilter = (filter) => {
+                    const leaderId = queryParams.get('leaderId');
+                    const leaderName = queryParams.get('leaderName');
+                    const developerId = queryParams.get('developerId');
+                    const developerName = queryParams.get('developerName');
+                    if (leaderId) {
+                        mixinFuncs.setQueryParams(
+                            serializeParams({ leaderId: leaderId, leaderName: leaderName, ...filter }),
+                        );
+                    } else if (developerId) {
+                        mixinFuncs.setQueryParams(
+                            serializeParams({ developerId: developerId, developerName: developerName, ...filter }),
+                        );
+                    } else {
+                        mixinFuncs.setQueryParams(serializeParams(filter));
+                    }
+                };
+                funcs.additionalActionColumnButtons = () => ({
+                    task: ({ id, name, state }) => (
+                        <BaseTooltip title={translate.formatMessage(commonMessage.task)}>
+                            <Button
+                                type="link"
+                                disabled={state === 1}
+                                style={{ padding: 0 }}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+
+                                    navigate(
+                                        routes.projectStudentTaskListPage.path + `?projectId=${id}&projectName=${name}`,
+                                    );
+                                }}
+                            >
+                                <BookOutlined />
+                            </Button>
+                        </BaseTooltip>
+                    ),
+                    member: ({ id, name }) => (
+                        <BaseTooltip title={translate.formatMessage(commonMessage.member)}>
+                            <Button
+                                type="link"
+                                style={{ padding: '0' }}
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    navigate(
+                                        routes.projectStudentMemberListPage.path +
+                                            `?projectId=${id}&projectName=${name}`,
+                                    );
+                                }}
+                            >
+                                <UserOutlined />
+                            </Button>
+                        </BaseTooltip>
+                    ),
+                });
+            },
+        });
+
+    const convertDate = (date) => {
+        const dateConvert = convertStringToDateTime(date, DEFAULT_FORMAT, DATE_FORMAT_DISPLAY);
+        return convertDateTimeToString(dateConvert, DATE_FORMAT_DISPLAY);
+    };
+
+    const columns = [
+        {
+            title: '#',
+            dataIndex: 'avatar',
+            align: 'center',
+            width: 80,
+            render: (avatar) => (
+                <AvatarField
+                    size="large"
+                    icon={<UserOutlined />}
+                    src={avatar ? `${AppConstants.contentRootUrl}${avatar}` : null}
+                />
+            ),
+        },
+        {
+            title: translate.formatMessage(commonMessage.fullName),
+            dataIndex: 'name',
+        },
+        {
+            title: translate.formatMessage(commonMessage.leader),
+            dataIndex: ['leaderInfo', 'leaderName'],
+            width: 150,
+        },
+        {
+            title: translate.formatMessage(message.startDate),
+            dataIndex: 'startDate',
+            render: (startDate) => {
+                return <div style={{ padding: '0 4px', fontSize: 14 }}>{convertDate(startDate)}</div>;
+            },
+            width: 140,
+            align: 'center',
+        },
+        {
+            title: translate.formatMessage(commonMessage.endDate),
+            dataIndex: 'endDate',
+            render: (endDate) => {
+                return <div style={{ padding: '0 4px', fontSize: 14 }}>{convertDate(endDate)}</div>;
+            },
+            width: 140,
+            align: 'center',
+        },
+        {
+            title: 'Tình trạng',
+            dataIndex: 'state',
+            align: 'center',
+            width: 120,
+            render(dataRow) {
+                const state = stateValues.find((item) => item.value == dataRow);
+                return (
+                    <Tag color={state.color}>
+                        <div style={{ padding: '0 4px', fontSize: 14 }}>{state.label}</div>
+                    </Tag>
+                );
+            },
+        },
+
+        // mixinFuncs.renderStatusColumn({ width: '120px' }),
+
+        mixinFuncs.renderActionColumn({ member: true, task: true }, { width: '120px' }),
+    ];
+
+    return (
+        <PageWrapper routes={[{ breadcrumbName: translate.formatMessage(commonMessage.project) }]}>
+            <ListPage
+                title={<span style={{ fontWeight: 'normal' }}>{leaderName || developerName}</span>}
+                baseTable={
+                    <BaseTable
+                        onChange={changePagination}
+                        pagination={pagination}
+                        loading={loading}
+                        dataSource={data}
+                        columns={columns}
+                    />
+                }
+            />
+        </PageWrapper>
+    );
+};
+
+export default ProjectStudentListPage;
