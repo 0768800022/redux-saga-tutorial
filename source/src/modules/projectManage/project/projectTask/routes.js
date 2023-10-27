35a366765d1d900412ebb14acbd6dd9aeea4a8c9
Nguyen Van Son <son17112001@gmail.com>
2023-10-27 15:03:43 +0700

    config leader page

--- source/src/modules/projectManage/project/projectTask/routes.js
+++ source/src/modules/projectManage/project/projectTask/routes.js
@@ -60,7 +60,7 @@ export default {
         path: '/project-leader/member/member-activity-project',
         title: 'Member Activity Project Leader List Page',
         auth: true,
-        component: MemberActivityProjectLeaderListPage,
+        component: MemberActivityProjectListPage,
         permissions: [apiConfig.projectTaskLog.getList.baseURL],
     },
 };
