1471b371bc9ecba429205b7cd5f1c457dd4c9621
Edward3103 <86288625+Edward3103@users.noreply.github.com>
2024-05-03 09:20:54 +0700

    login ony admin

--- source/src/hooks/useMoneyUnit.js
+++ source/src/hooks/useMoneyUnit.js
@@ -4,8 +4,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 const useMoneyUnit = () => {
     const settingSystem = useSelector(settingSystemSelector);
-    const moneyUnit = settingSystem?.find((item) => item.keyName === settingKeyName.MONEY_UNIT);
-    return moneyUnit.valueData;
+    const moneyUnit = settingSystem?.find((item) => item?.keyName === settingKeyName.MONEY_UNIT);
+    return moneyUnit?.valueData;
 };
 
 export default useMoneyUnit;
