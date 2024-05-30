892741755c65b867e219e8e1b662aafa07bcce63
Vttien01 <tientienpham01@gmail.com>
2024-05-30 14:07:18 +0700

    add button excel

--- source/src/components/common/elements/StatisticsTaskModal.js
+++ source/src/components/common/elements/StatisticsTaskModal.js
@@ -1,6 +1,6 @@
-import { DATE_DISPLAY_FORMAT } from '@constants';
+import { DATE_DISPLAY_FORMAT, DEFAULT_EXCEL_DATE, storageKeys } from '@constants';
 import useDisclosure from '@hooks/useDisclosure';
-import { Modal, Tag, Tooltip } from 'antd';
+import { Button, Flex, Modal, Space, Tag, Tooltip } from 'antd';
 import React from 'react';
 import BaseTable from '../table/BaseTable';
 import useTranslate from '@hooks/useTranslate';
@@ -10,12 +10,19 @@ import { stateResgistrationOptions } from '@constants/masterData';
 import { convertMinuteToHour, formatMoney, formatMoneyValue } from '@utils';
 import useTrainingUnit from '@hooks/useTrainingUnit';
 import styles from './modal.module.scss';
+import { FileExcelOutlined } from '@ant-design/icons';
+import { FormattedMessage } from 'react-intl';
+import { getData } from '@utils/localStorage';
+import { showSucsessMessage } from '@services/notifyService';
+import { getCacheAccessToken } from '@services/userService';
+import axios from 'axios';
 
 const StatisticsTaskModal = ({ detail, open, close }) => {
     const [openedStateTaskModal, handlersStateTaskModal] = useDisclosure(false);
     const translate = useTranslate();
     const stateRegistration = translate.formatKeys(stateResgistrationOptions, ['label']);
     const { trainingUnit, bugUnit } = useTrainingUnit();
+    const userAccessToken = getCacheAccessToken();
     const formatPercentValue = (value) => {
         return formatMoney(value, {
             groupSeparator: ',',
@@ -67,9 +74,9 @@ const StatisticsTaskModal = ({ detail, open, close }) => {
                         }
                     >
                         <div
-                            // className={classNames(
-                            //     value > trainingUnit ? styles.customPercent : styles.customPercentOrange,
-                            // )}
+                        // className={classNames(
+                        //     value > trainingUnit ? styles.customPercent : styles.customPercentOrange,
+                        // )}
                         >
                             {value > 0 ? (
                                 <div>-{formatPercentValue(parseFloat(value))}</div>
@@ -115,9 +122,9 @@ const StatisticsTaskModal = ({ detail, open, close }) => {
                         }
                     >
                         <div
-                            // className={classNames(
-                            //     value > bugUnit ? styles.customPercent : styles.customPercentOrange,
-                            // )}
+                        // className={classNames(
+                        //     value > bugUnit ? styles.customPercent : styles.customPercentOrange,
+                        // )}
                         >
                             {value > 0 ? (
                                 <div>-{formatPercentValue(parseFloat(value))}</div>
@@ -149,9 +156,56 @@ const StatisticsTaskModal = ({ detail, open, close }) => {
             },
         },
     ];
+    const exportToExcel = (value, nameExcel) => {
+        axios({
+            url: `${getData(storageKeys.TENANT_API_URL)}/v1/salary-period/export-to-excel/${value}`,
+            method: 'GET',
+            responseType: 'blob',
+            // withCredentials: true,
+            headers: {
+                Authorization: `Bearer ${userAccessToken}`, // Sử dụng token từ state
+            },
+        })
+            .then((response) => {
+                // const fileName="uy_nhiem_chi";
+                const date = new Date();
+
+                const excelBlob = new Blob([response.data], {
+                    type: response.headers['content-type'],
+                });
+
+                const link = document.createElement('a');
+
+                link.href = URL.createObjectURL(excelBlob);
+                link.download = `KyLuong_${nameExcel}.xlsx`;
+                link.click();
+                showSucsessMessage('Tạo tệp ủy nhiệm chi thành công');
+            })
+            .catch((error) => {
+                console.log(error);
+                // Xử lý lỗi tải file ở đây
+            });
+    };
     return (
         <Modal
-            title="Thống kê các task hoàn thành"
+            title={
+                <div>
+                    <Space>Thống kê các task hoàn thành</Space>
+                    <Tooltip title={<FormattedMessage defaultMessage={'Export'} />}>
+                        <Button
+                            // disabled={state === PAYOUT_PERIOD_STATE_DONE}
+                            type="link"
+                            style={{ padding:0, marginTop:'-5px', marginLeft:10, display: 'table-cell', verticalAlign: 'middle' }}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                // exportToExcel(id, name);
+                            }}
+                        >
+                            <FileExcelOutlined style={{ color: 'green' }} size={18} />
+                        </Button>
+                    </Tooltip>
+                </div>
+            }
             open={open}
             destroyOnClose={true}
             footer={null}
