369cd62ca4ff03ba6dd217af7fe4938979cdf0b1
0768800022 <huycuong140203@gmail.com>
2024-08-20 10:07:29 +0700

    redux actions

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://mithril-rem.fly.dev/api';
-
+//axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/https://rem.dbwebb.se/api';
 //axios.get('/users');
+
+
+const store = createStore(reducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
