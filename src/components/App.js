1ec84282b99d4499e5b43af744a9f18b343579ae
0768800022 <huycuong140203@gmail.com>
2024-08-20 10:49:33 +0700

    generator function

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,21 @@
 
+function* testing() {
+  while(true){
+    yield 1; //tạm dừng và trả về 1
+    yield 2;
+    yield 3;
+  }
+}
+
 function App() {
+  const iterator = testing();
+
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  
+
   return (
     <div>
       Redux-saga with React
