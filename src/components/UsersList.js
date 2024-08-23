07641144cbfc5ca4d0a76b0376ea8a820e0628a3
0768800022 <huycuong140203@gmail.com>
2024-08-23 15:52:47 +0700

    fix Modal

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,5 @@
 import { Button, List, Modal, Input, Form } from "antd";
-import { useState } from "react";
+import { useId, useState } from "react";
 
 const UsersList = ({ users, onDeleteUser, onEditUser }) => {
 
@@ -29,9 +29,14 @@ const UsersList = ({ users, onDeleteUser, onEditUser }) => {
         handleShow.close();
     };
 
-    const handleDelete = () => {
+    const handleDelete = (user) => {
         if (deleteInModal) {
-            onDeleteUser(deleteInModal);
+            onDeleteUser({
+                ...deleteInModal,
+                // // user: user.id,
+                // firstName: user.firstName,
+                // lastName: user.lastName,
+            });
             hideModal();
         }
     };
@@ -60,88 +65,90 @@ const UsersList = ({ users, onDeleteUser, onEditUser }) => {
     
 
     return (
-        <List>
-            {users.sort((a, b) => {
-                if (a.firstName > b.firstName) {
-                    return 1;
-                } else if (a.firstName < b.firstName) {
-                    return -1;
-                } else if (a.lastName > b.lastName) {
-                    return 1;
-                } else if (a.lastName < b.lastName) {
-                    return -1;
-                } else {
-                    return 0;
-                }
-            }).map((user) => {
-                return (
-                    <List.Item key={user.id} style={{ border: '1px solid #333', borderRadius: '20px' }}>
-                        <section style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
-                            <div style={{ flex: 1 }}>
-                                <span style={{ marginLeft: '5px' }}>{user.firstName}</span>
-                                <span style={{ marginLeft: '5px' }}>{user.lastName}</span>
-                            </div>
-                            <div style={{ marginRight: '5px' }}>
-                                <Button type="primary" onClick={() => showModal(user.id)} danger>
-                                    Delete
-                                </Button>
-                                <Modal
-                                    title="Bạn có chắc chắn muốn xóa không?"
-                                    open={open && deleteInModal === user.id}
-                                    onOk={() => {
-                                        handleDelete();
-                                        onDeleteUser(user.id);
-                                        hideModal();
-                                    }}
-                                    onCancel={hideModal}
-                                    okText="Delete"
-                                    cancelText="Cancel"
-                                >
-                                    <p>  </p>
-                                </Modal>
-                            </div>
+        <>
+            <List>
+                {users.sort((a, b) => {
+                    if (a.firstName > b.firstName) {
+                        return 1;
+                    } else if (a.firstName < b.firstName) {
+                        return -1;
+                    } else if (a.lastName > b.lastName) {
+                        return 1;
+                    } else if (a.lastName < b.lastName) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }).map((user) => {
+                    return (
+                        <List.Item key={user.id} style={{ border: '1px solid #333', borderRadius: '20px' }}>
+                            <section style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+                                <div style={{ flex: 1 }}>
+                                    <span style={{ marginLeft: '5px' }}>{user.firstName}</span>
+                                    <span style={{ marginLeft: '5px' }}>{user.lastName}</span>
+                                </div>
+                                <div style={{ marginRight: '5px' }}>
+                                    <Button type="primary" onClick={() => showModal(user.id)} danger>
+                                        Delete
+                                    </Button>
+                                    
+                                </div>
 
-                            <div style={{ marginRight: '5px' }}>
-                                <Button type="primary" onClick={() => showEditModal(user)}>
-                                    Edit
-                                </Button>
-                                <Modal
-                                    title="Chỉnh sửa thông tin người dùng"
-                                    open={openEdit && editInModal?.id === user.id}
-                                    onOk={() => {
-                                        editForm.submit(); // Nộp dữ liệu từ form chỉnh sửa
-                                    }}
-                                    onCancel={hideEditModal}
-                                    okText="Save"
-                                    cancelText="Cancel"
-                                >
-                                    <Form
-                                        form={editForm}
-                                        layout="vertical"
-                                        onFinish={handleEdit}
-                                    >
-                                        <Form.Item
-                                            label="First Name"
-                                            name="firstName"
-                                            rules={[{ required: true, message: 'Please input first name!' }]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                        <Form.Item
-                                            label="Last Name"
-                                            name="lastName"
-                                            rules={[{ required: true, message: 'Please input last name!' }]}
-                                        >
-                                            <Input />
-                                        </Form.Item>
-                                    </Form>
-                                </Modal>
-                            </div>
-                        </section>
-                    </List.Item>
-                );
-            })}
-        </List>
+                                <div style={{ marginRight: '5px' }}>
+                                    <Button type="primary" onClick={() => showEditModal(user)}>
+                                        Edit
+                                    </Button>
+                                    
+                                </div>
+                            </section>
+                        </List.Item>
+                    );
+                })}
+            </List>
+
+
+             <Modal
+                title="Bạn có chắc chắn muốn xóa không?"
+                open={open}
+                onOk={() => {
+                    handleDelete();
+                    onDeleteUser();
+                    hideModal();
+                }}
+                onCancel={hideModal}
+                okText="Delete"
+                cancelText="Cancel"
+            >
+                <p>Bạn có chắc chắn muốn xóa người dùng này?</p>
+            </Modal>
+
+            <Modal
+                title="Chỉnh sửa thông tin người dùng"
+                open={openEdit}
+                onOk={() => editForm.submit()}
+                onCancel={hideEditModal}
+                okText="Save"
+                cancelText="Cancel"
+            >
+                <Form form={editForm} layout="vertical" onFinish={handleEdit}>
+                    <Form.Item
+                        label="First Name"
+                        name="firstName"
+                        rules={[{ required: true, message: 'Please input first name!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Last Name"
+                        name="lastName"
+                        rules={[{ required: true, message: 'Please input last name!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </>
+        
     );
 };
 
